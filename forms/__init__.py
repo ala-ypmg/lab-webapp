29a7f58b03febfaca98b8bd60a769fd3d5676e10
Gale <ala@ypmg.com>
2026-04-14 16:08:45 -0700

    Initialize forms module with form imports

---
+++ forms/__init__.py
@@ -0,0 +1,6 @@
+from .login_form import LoginForm
+from .workflow_form import WorkflowForm
+from .notes_form import NotesForm
+from .register_form import RegistrationForm, ResendConfirmationForm
+
+__all__ = ['LoginForm', 'WorkflowForm', 'NotesForm', 'RegistrationForm', 'ResendConfirmationForm']

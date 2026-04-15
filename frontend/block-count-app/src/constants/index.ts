7cb614c2f359a9093dde03580757c9787b611b4c
Gale <ala@ypmg.com>
2026-04-15 08:28:06 -0700

    Add constants index file for exports

---
+++ frontend/block-count-app/src/constants/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Constants Index
+ */
+
+export { FACILITY_PREFIX_MAP, PREFIX_TO_FACILITY, ALL_PREFIXES, ALL_FACILITIES, PREFIXES_BY_FACILITY } from './facilities';
+export { TOOLTIPS } from './tooltips';
+export { theme, colors } from './theme';

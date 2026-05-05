b27d769f6a23ec232100a29a5a7e3defe45b2c92
Gale <ala@ypmg.com>
2026-05-05 14:56:34 -0700

    Import shake.css for styling

--- frontend/accessioning-workflow/src/main.tsx
+++ frontend/accessioning-workflow/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './shake.css';
 import App from './App.tsx';
 
 const el = document.getElementById('root');
